2ffb90b66dddc1ae5c6e91f0f5cd6b4e9ccd53b8
sunidhich25 <sunidhichowdhary@Sunidhis-MacBook-Air-2.local>
2026-06-04 15:40:08 +0530

    changed the relative path

--- ML/model.py
+++ ML/model.py
@@ -2,35 +2,31 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import cv2
-
+import os
 CLASSES    = ["straight", "textured"]
 IMG_SIZE   = 224
-MODEL_PATH = "models/hair_mobilenetv3.keras"
+MODEL_PATH = os.path.join(os.path.dirname(__file__),"models","hair_mobilenetv3.keras")
 
-# Load model once when file is imported
-# Loading takes ~1 second — you don't want to reload every request
+
 model = keras.models.load_model(MODEL_PATH)
 
 def detect_hair_texture(image) -> dict:
-    """
-    Input:  path to a face/hair photo
-    Output: { hair_texture, confidence, all_scores }
-    """
+   
     img = cv2.imread(image)
     if img is None:
         return {"error": "Could not read image"}
 
-    # Preprocess EXACTLY as during training
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   # BGR → RGB
-    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))   # resize to 224x224
-    img = img / 255.0                             # normalise to 0-1
+    
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   
+    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))   
+    img = img / 255.0                             
     img = np.expand_dims(img, axis=0)             # add batch dim: (224,224,3) → (1,224,224,3)
 
     # Run prediction
-    preds = model.predict(img, verbose=0)   # shape: (1, 4)
-    probs = preds[0]                        # shape: (4,) — one prob per class
+    preds = model.predict(img, verbose=0)   
+    probs = preds[0]                       
 
-    idx        = int(np.argmax(probs))      # index of highest probability
+    idx        = int(np.argmax(probs))     
     hair_type  = CLASSES[idx]
     confidence = round(float(probs[idx]), 2)
 
